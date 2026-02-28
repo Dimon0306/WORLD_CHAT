b7e9e295f251c1095b3cc7ad438b3bbbb5c75515
Dimon0306 <pisarevskiy.da@bk.ru>
2026-03-01 00:23:19 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,6 +24,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
     
 templates = Jinja2Templates(directory="templates")
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -153,3 +155,4 @@ async def websocket_endpoint(websocket: WebSocket):
     except:
 
         active_connections[:] = [c for c in active_connections if c["websocket"] != websocket]
+
